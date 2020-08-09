e45e35d3b0c9e147a313bdace8c5166ab2825478
Yoyo <rodrigo.diazde@sansano.usm.cl>
2020-08-09 19:59:59 -0400

    sas

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const endpoints = {
-    development: '<http://iso-solrec.herokuapp.com>',
+    development: 'http://iso-solrec.herokuapp.com',
 };
 
 export const api = axios.create({
