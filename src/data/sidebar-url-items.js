a4c175aa2486ce294ece148a6535eb26e03ccfcc
Yoyo <rodrigo.diazde@sansano.usm.cl>
2020-08-11 10:08:33 -0400

    new

--- src/data/sidebar-url-items.js
+++ src/data/sidebar-url-items.js
@@ -1,10 +1,5 @@
 export default function() {
     return [
-      {
-        title: "Youtube",
-        to: "https://www.youtube.com/",
-        htmlBefore: '<i class="material-icons">ondemand_video</i>',
-      },
     ];
   }
   
