c64bf6cb83b43c8e0f61e870f94f834d59efb480
Yoyo <rodrigo.diazde@sansano.usm.cl>
2020-08-11 16:23:10 -0400

    youtu again

--- src/data/sidebar-url-items.js
+++ src/data/sidebar-url-items.js
@@ -1,5 +1,9 @@
 export default function() {
     return [
+      {
+        title: "Youtube",
+        to: "https://www.youtube.com/",
+        htmlBefore: '<i class="material-icons">ondemand_video</i>',
+      },
     ];
   }
-  
